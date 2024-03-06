a39b27463189230dcb40a9b2f1dccafeb5b0265d
Kim_mina <mina14712@gmail.com>
2024-03-07 01:21:13 +0900

    0307_try_1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// logo hover시 색상 변환
+
+document.querySelector('.top-logo').addEventListener('click',function () {
+  // document.querySelector('.logo-place .top-logo').computedStyleMap 외안되?ㅠㅠ
+  document.getElementsByClassName('top-logo');
+  // document.querySelector('.top-logo').classList.add('.display-none');
+});
+
+// document.getElementById('test').style.width = '1000px';
+document.querySelector('#test') // querySelector로는 스타일 접근 못하나요 ㅠ?
+
+let slideIndex = 1;
+
+setInterval(function () {
+},2500);
