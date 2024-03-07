777ab5b26251aa9792af97e0c47193ca02e64aca
kim_mina <mina14712@gmail.com>
2024-03-07 11:10:16 +0900

    0307

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 // logo hover시 색상 변환
+const topLogo = document.querySelector('.logo-place .top-logo');
 
-document.querySelector('.top-logo').addEventListener('click',function () {
-  // document.querySelector('.logo-place .top-logo').computedStyleMap 외안되?ㅠㅠ
-  document.getElementsByClassName('top-logo');
-  // document.querySelector('.top-logo').classList.add('.display-none');
+topLogo.addEventListener('mouseenter',function () {
+  topLogo.style.display="none";
 });
-
-// document.getElementById('test').style.width = '1000px';
-document.querySelector('#test') // querySelector로는 스타일 접근 못하나요 ㅠ?
+topLogo.addEventListener('blur',function () {
+  topLogo.style.display="block";
+});
 
 let slideIndex = 1;
 
