8bf923adcc45edf3aaeded34cd55181b1c8829bf
kim_mina <mina14712@gmail.com>
2024-03-18 11:42:02 +0900

    0318

--- js/script.js
+++ js/script.js
@@ -44,3 +44,71 @@ new Swiper('.swiper',{
 });
 
 
+// badge - scroll
+const badgeEl = document.querySelector('.badge');
+const badgeUp = badgeEl.querySelector('.up-to');
+const badgeDown = badgeEl.querySelector('.down-to');
+
+window.addEventListener('scroll',function () {
+  if( this.window.scrollY > 100 ){
+    badgeEl.style.visibility = 'visible';
+    badgeEl.style.opacity = 1;
+
+  } else {
+    badgeEl.style.visibility = 'hidden';
+    badgeEl.style.opacity = 0;
+  }
+});
+
+//badge - up down
+badgeUp.addEventListener('click', function () {
+  if (window.scrollY != 0) {
+    window.scrollTo({top:0, behavior:"smooth"});
+  }
+});
+badgeDown.addEventListener('click', function () {
+  window.scrollTo({ top:9200, behavior:"smooth"});
+});
+// 질문 7 scrollTo bottom같은건 왜 없을까요 .. 이게 최선의 방법일까요...?
+
+// HEADER scroll내릴시 고정
+// HEADER scroll내릴시 top-logo scale 변환
+const headerEl = document.querySelector('header');
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 50) {
+    headerEl.classList.add('header-style');
+    headerEl.querySelector('.top-logo').style.scale = '80%';
+    headerEl.querySelector('.top-logo-gray').style.scale = '80%';
+
+  } else {
+    headerEl.classList.remove ('header-style');
+    headerEl.querySelector('.top-logo').style.scale = '100%';
+    headerEl.querySelector('.top-logo-gray').style.scale = '100%';
+  }
+})
+// featured - 호버시 다른상품 보이게
+const featuredBtnEls = document.querySelectorAll('.featured-button a');
+const featuredGroupEls = document.querySelectorAll('.featured-group');
+// document.querySelectorAll('.featured-button a').forEach((featuredBtnEl,index) => {
+  // featuredBtnEl[1].addEventListener('mouseenter', () => {
+  //   document.querySelectorAll('.featured-group').forEach((featuredGroupEl,index_2) => {
+  //     featuredGroupEl.style.display = 'none';
+  //     featuredGroupEl[1].style.display = 'block'; <- 질문 7 이게 안되용,,, 
+  //   });
+  // });
+  featuredBtnEls.forEach((featuredBtnEl,index) => {
+    featuredGroupEls[0].style.display = 'flex';
+  featuredBtnEls[index].addEventListener('mouseenter', () => {
+    featuredGroupEls.forEach((featuredGroupEl) => {
+      featuredGroupEl.style.display = 'none';
+      featuredGroupEls[index].style.display = 'flex';
+    });
+  });
+
+});
+
+// COUSTOM REVIEW 버튼 눌러 슬라이드
+const reviewBtnL = document.querySelector('.buttom_left');
+const reviewBtnR = document.querySelector('.buttom_right');
+const reviewPage = document.querySelectorAll('.review-each');
+
